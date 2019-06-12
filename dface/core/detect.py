b25ee7eaf1ef46c7bf15cf65447a8f4c5797259f
Rajitha Prabath <ratmcu@gmail.com>
2019-06-12 14:48:02 -0400

    removed the r and p net

--- dface/core/detect.py
+++ dface/core/detect.py
@@ -605,32 +605,33 @@ face candidates:%d, current batch_size:%d"%(num_boxes, batch_size)
         t = time.time()
 
         # pnet
-        if self.pnet_detector:
-            boxes, boxes_align = self.detect_pnet(img)
-            if boxes_align is None:
-                return np.array([]), np.array([])
+        # if self.pnet_detector:
+        #     boxes, boxes_align = self.detect_pnet(img)
+        #     if boxes_align is None:
+        #         return np.array([]), np.array([])
 
-            t1 = time.time() - t
-            t = time.time()
+        #     t1 = time.time() - t
+        #     t = time.time()
 
         # rnet
-        if self.rnet_detector:
-            boxes, boxes_align = self.detect_rnet(img, boxes_align)
-            if boxes_align is None:
-                return np.array([]), np.array([])
+        # if self.rnet_detector:
+        #     boxes, boxes_align = self.detect_rnet(img, boxes_align)
+        #     if boxes_align is None:
+        #         return np.array([]), np.array([])
 
-            t2 = time.time() - t
-            t = time.time()
+        #     t2 = time.time() - t
+        #     t = time.time()
 
         # onet
         if self.onet_detector:
+            print('using the onet detector')
             boxes_align, landmark_align = self.detect_onet(img, boxes_align)
             if boxes_align is None:
                 return np.array([]), np.array([])
 
             t3 = time.time() - t
             t = time.time()
-            print("time cost " + '{:.3f}'.format(t1+t2+t3) + '  pnet {:.3f}  rnet {:.3f}  onet {:.3f}'.format(t1, t2, t3))
+            #print("time cost " + '{:.3f}'.format(t1+t2+t3) + '  pnet {:.3f}  rnet {:.3f}  onet {:.3f}'.format(t1, t2, t3))
 
         return boxes_align, landmark_align
 
